a8b5b3f9216b17d5c2bb4f6a216dfd0aa80ab929
SolitaryDev <105029593+SolitaryDev001@users.noreply.github.com>
2022-05-09 15:04:27 -0400

    Update problema_trabajadores_incompleto.py

--- 09-05-2022/problema_trabajadores_incompleto.py
+++ 09-05-2022/problema_trabajadores_incompleto.py
@@ -3,7 +3,7 @@
 # nombre, --> Listo
 # edad (generada en forma aleatoria en el  rango 18 a 80 años), 
 # tipo de contrato (Licenciado, Técnico u Obrero en forma aleatoria) y 
-# la cantidad de horas trabajadas. 
+# la cantidad de horas trabajadas. --> Listo
 # El valor de la hora depende del tipo 
 # de contrato según tabla:
 # Tipo de contrato	Valor hora
@@ -13,9 +13,9 @@
 
 # El programa debe mostrar:
 # •	Edad, tipo de contrato y Sueldo de cada trabajador (valor hora x 
-# cantidad de horas trabajadas) --> En el ciclo principal???
+# cantidad de horas trabajadas) --> En el ciclo principal ---> lista
 # •	Nombre y Edad del trabajador con mayor sueldo --> al final
-# •	Total pagado por parte de la empresa a sus trabajadores --> al final
+# •	Total pagado por parte de la empresa a sus trabajadores --> al final -->Listo
 # •	Cantidad trabajadores con más de 60 años --> al final
 
 # Mi problema tiene tres partes: 
@@ -47,6 +47,13 @@ while(not valido ): # mientras el ingreso no sea valido
 
 print('Hay ' , n , ' trabajadores')
 
+# variables para la estadistica:
+contador_mayores60 = 0
+suma_sueldos = 0
+mayor_sueldo = 0
+nombre_mayor_sueldo = ""
+edad_mayor_sueldo = 0
+
 
 # -----------------------------------------------------------
 # Parte 2 : ciclo principal...
@@ -59,19 +66,62 @@ while(contador_trabajadores < n ) :
 		nombre = input("Ingrese el nombre del trabajador : ")
 		if (len(nombre) == 0):
 			print("El nombre no puede quedar vacio.")
-			
+
 	# Leemos la cantidad de horas trabajadas: 
 	valido = False
 	while(not valido ): # mientras el ingreso no sea valido 
 		try:
-			n = int(input("Ingrese el número horas trabajadas : "))
-			if(n > 0):
+			horas_trabajadas = int(input("Ingrese el número horas trabajadas : "))
+			if(horas_trabajadas > 0):
 				valido = True
 			else:
 				print("El numero debe ser entero y positivo ")
 		except ValueError: #en caso de que el try no se pueda hacer
 			print("Error, debe ingresar un numero entero y positivo")
 
-	# continuamos con el proceso:
+
+	# Generamos al azar la edad y el tipo de contrato :
+	edad = random.randint(18,80)
+	tipo_contrato = random.randint(1,3)
+	# Asignamos el contrato y valor hora correspondiente
+	contrato = ""
+	valor_hora = 0
+	if tipo_contrato == 1 :
+		contrato = "Licenciado"
+		valor_hora = 10250 
+	elif tipo_contrato == 2  :
+		contrato = "Tecnico"
+		valor_hora = 7120
+	elif tipo_contrato == 3  :
+		contrato = "Obrero"
+		valor_hora = 5200
+	# calculamos cuanto hay que pagarle	
+	sueldo = horas_trabajadas * valor_hora
+	# imprimir los detalles del trabajador:
 	print("nombre : " , nombre)
+	print("edad : " , edad)
+	print("tipo contrato : " , tipo_contrato)
+	print("contrato : " , contrato)
+	print("valor hora : ", valor_hora )
+	print("horas trabajadas :" , horas_trabajadas )
+	print("sueldo : " , sueldo)
+
+	# actualizamos los valores para la estadistica. 
+	suma_sueldos = suma_sueldos + sueldo
+	if edad > 60 :
+		contador_mayores60 = contador_mayores60 + 1
+	if sueldo > mayor_sueldo :
+		mayor_sueldo = sueldo
+		edad_mayor_sueldo = edad
+		nombre_mayor_sueldo = nombre
+
+	# aumento el contador para continuar el ciclo principal
 	contador_trabajadores = contador_trabajadores + 1
+
+# -----------------------------------------------------------
+# Parte 3 : Mostrar el resultado.
+# -----------------------------------------------------------
+print("La suma de los sueldos es : $" , suma_sueldos)
+print("Hay  " , contador_mayores60 , " trabajadores mayores de 60 años")
+print("El trabajador con mayor sueldo es : " , nombre_mayor_sueldo )
+print("Y su edad es ", edad_mayor_sueldo)
