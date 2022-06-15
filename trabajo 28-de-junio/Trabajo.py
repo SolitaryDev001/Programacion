088bf88410a66973df2f3f0de9a85dfbcf3d09ca
SolitaryDev <105029593+SolitaryDev001@users.noreply.github.com>
2022-06-14 21:58:52 -0400

    Create Trabajo.py

---
+++ trabajo 28-de-junio/Trabajo.py
@@ -0,0 +1,16 @@
+from Libraria import *;
+
+asientos=generarmatriz(6);
+pasajeros=generarmatriz(6);
+num=0;
+for x in range(6):
+    for y in range(6):
+        num += 1;
+        rellenarmatriz(asientos, x, y, num);
+num=0;
+for x in range(6):
+    for y in range(6):
+        num += 1;
+        rellenarmatriz(pasajeros, x, y, num);
+
+menu(asientos, pasajeros);
